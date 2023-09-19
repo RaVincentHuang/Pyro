2e286c0064b113144ec6bc6fd6cd59983e1627c2
Vincent <18573104293@163.com>
2023-09-19 12:49:36 +0800

    Trie

--- include/Trie.h
+++ include/Trie.h
@@ -1,11 +1,21 @@
 #ifndef TRIE_H
 #define TRIE_H
 
+#include <boost/dynamic_bitset/dynamic_bitset.hpp>
 #include <cstddef>
+#include <functional>
 #include <memory>
+#include <type_traits>
 #include <vector>
 #include <boost/dynamic_bitset.hpp>
-template<typename ValueType>
+
+using BitSet = boost::dynamic_bitset<size_t>;
+
+// template<typename ValueType>
+//     concept shared_ptr = std::is_;
+
+template<typename ValueType> // need to be std::shared_ptr
+// TODO use concept
 class Trie {
     int offset;
     size_t dimension;
@@ -23,8 +33,40 @@ class Trie {
             subTries = std::vector<std::unique_ptr<Trie<ValueType>>>(dimension - offset, nullptr);
         }
 
-        auto subTrie = getSubTrie(index);
-        
+        Trie<ValueType>* subTrie = getSubTrie(index);
+        if(subTrie == nullptr) {
+            subTries.at(index - offset) = std::make_unique<Trie<ValueType>>(index + 1, dimension);
+            subTrie = subTries.at(index - offset).get();
+        }
+        return subTrie;
+    }
+
+    void traverseEntries(BitSet& subsetKey, std::function<void(BitSet&, ValueType)> collector) {
+        if(value != nullptr) {
+            collector(BitSet(subsetKey), value);
+        }
+
+        for(size_t i = offset; i < dimension; i++) {
+            Trie<ValueType>* subTrie = getSubTrie(i);
+            if(subTrie != nullptr) {
+                subsetKey.set(i);
+                subTrie->traverseEntries(subsetKey, collector);
+                subsetKey.reset(i);
+            }
+        }
+    }
+
+    bool isEmpty() {
+        if(value != nullptr)
+            return false;
+        if(subTries.empty())
+            return true;
+
+        for(auto subTrie : this->subTries) {
+            if(subTrie != nullptr)
+                return false;
+        }
+        return true;
     }
 
 public:
@@ -33,17 +75,152 @@ public:
     Trie(int offset, size_t dimension)
         : offset(offset), dimension(dimension) {}
     
-    ValueType associate(const boost::dynamic_bitset<size_t>& key, size_t nextBit, ValueType value) {
+    ValueType associate(const BitSet& key, size_t nextBit, ValueType value) {
         nextBit = key.find_next(nextBit);
-        if(nextBit == boost::dynamic_bitset<>::npos) {
+        if(nextBit == BitSet::npos) {
             ValueType oldValue = this->value;
             this->value = value;
             return oldValue;
         }
 
-
+        return getOrCreateSubTrie(nextBit)->associate(key, nextBit + 1, value);
     }
 
+    ValueType get(const BitSet& key, size_t nextBit) {
+        nextBit = key.find_next(nextBit);
+        if(nextBit == BitSet::npos) {
+            return value;
+        }
+
+        Trie<ValueType>* subTrie = getSubTrie(nextBit);
+        if(subTrie == nullptr)
+            return nullptr;
+
+        return subTrie->get(key, nextBit + 1);
+    }
+
+    ValueType remove(const BitSet& key, size_t nextBit) {
+        nextBit = key.find_next(nextBit);
+        if(nextBit == BitSet::npos) {
+            ValueType removedValue = value;
+            value = nullptr;
+            return removedValue;
+        }
+
+        Trie<ValueType>* subTrie = getSubTrie(nextBit);
+        if(subTrie == nullptr)
+            return nullptr;
+
+        ValueType removedValue = subTrie->remove(key, nextBit + 1);
+        if(subTrie->isEmpty()) {
+            this->subTries[nextBit - this->offset] = nullptr;
+        }
+
+        return removedValue;
+    }
+
+    bool collectSubsetKeys(BitSet& key, size_t nextBit, BitSet& subsetKey, std::function<bool(BitSet&, ValueType)> collector) {
+        if(value != nullptr && !collector(BitSet(subsetKey), value)) {
+            return false;
+        }
+
+        nextBit = key.find_next(nextBit);
+        while (nextBit != BitSet::npos) {
+            Trie<ValueType>* subTrie = getSubTrie(nextBit);
+            if(subTrie != nullptr) {
+                subsetKey.set(nextBit);
+                if(!collectSubsetKeys(key, nextBit + 1, subsetKey, collector))
+                    return false;
+                subsetKey.reset(nextBit);
+            }
+
+            nextBit = key.find_next(nextBit + 1);
+        }
+
+        return true;
+    }
+
+    bool collectSupersetKeys(BitSet& key, size_t nextBit, BitSet& superKey, std::function<bool(BitSet&, ValueType)> collector) {
+        nextBit = nextBit == BitSet::npos ? nextBit : key.find_next(nextBit);
+
+        if(nextBit == BitSet::npos) {
+            if(value != nullptr && !collector(BitSet(superKey), value))
+                return false;
+
+            for(size_t i = offset; i < dimension; i++) {
+                Trie<ValueType>* subTrie = getSubTrie(i);
+                if(subTrie != nullptr) {
+                    superKey.set(i);
+                    if(!subTrie->collectSupersetKeys(key, nextBit, superKey, collector))
+                        return false;
+                    superKey.reset(i);
+                }
+            }
+        } else {
+            for(size_t i = offset; i < nextBit; i++) {
+                Trie<ValueType>* subTrie = getSubTrie(i);
+                if(subTrie != nullptr) {
+                    superKey.set(i);
+                    if(!subTrie->collectSupersetKeys(key, nextBit, superKey, collector))
+                        return false;
+                    superKey.reset(i);
+                }
+            }
+
+            Trie<ValueType>* subTrie = getSubTrie(nextBit);
+            if(subTrie != nullptr) {
+                superKey.set(nextBit);
+                if(!subTrie->collectSupersetKeys(key, nextBit + 1, superKey, collector))
+                    return false;
+                superKey.reset(nextBit);
+            }
+        }
+
+        return true;
+    }
+
+    bool collectRestrictedSupersetKeys(BitSet& key, BitSet& blackList, size_t nextBit, BitSet& superKey, std::function<bool(BitSet&, ValueType)> collector) {
+        nextBit = nextBit == BitSet::npos ? nextBit : key.find_next(nextBit);
+
+        if(nextBit == BitSet::npos) {
+            if(value != nullptr && !collector(BitSet(superKey), value))
+                return false;
+
+            for(size_t i = offset; i < dimension; i++) {
+                if(blackList.test(i))
+                    continue;
+                Trie<ValueType>* subTrie = getSubTrie(i);
+                if(subTrie != nullptr) {
+                    superKey.set(i);
+                    if(!subTrie->collectRestrictedSupersetKeys(key, blackList, nextBit, superKey, collector))
+                        return false;
+                    superKey.reset(i);
+                }
+            }
+        } else {
+            for(size_t i = offset; i < nextBit; i++) {
+                if(blackList.test(i))
+                    continue;
+                Trie<ValueType>* subTrie = getSubTrie(i);
+                if(subTrie != nullptr) {
+                    superKey.set(i);
+                    if(!subTrie->collectRestrictedSupersetKeys(key, blackList, nextBit, superKey, collector))
+                        return false;
+                    superKey.reset(i);
+                }
+            }
+
+            Trie<ValueType>* subTrie = getSubTrie(nextBit);
+            if(subTrie != nullptr) {
+                superKey.set(nextBit);
+                if(!subTrie->collectRestrictedSupersetKeys(key, blackList, nextBit + 1, superKey, collector))
+                    return false;
+                superKey.reset(nextBit);
+            }
+        }
+
+        return true;
+    }
 
 };
 
